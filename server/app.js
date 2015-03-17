3de9b638027cc6eee9255f5b89fab2b83783e903
Daman <damandhillon24@gmail.com>
2015-03-17 16:27:42 -0700

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var config = require('./config/environment');
+// Setup server
+var app = express();
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
+
+// Setup Sockets
+var io = require('socket.io').listen(server);
+
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  console.log("Server listening on port " + config.port + "$ IP " + config.ip );
+});
+
+// Expose app
+exports = module.exports = app;
